5921dd43a334ab9d9a57e4aecd2fe7a02ccff3f8
FolkFolk56 <6529011008@cdti.ac.th>
2024-07-01 16:27:37 +0700

    update

---
+++ lab5.1.py
@@ -0,0 +1,20 @@
+sum = 0
+
+def avg(num):
+   for i in range num: 
+        score += int(input("รับคะแนน %d :"% i)) 
+    sum = score/num
+    return sum
+
+
+
+n = int(input("รับจำนวนคน"))    
+print("คะแนนเฉลี่ย %.2f"% avg(n))    
+    
+    
+    
+    
+    
+    
+    
+    
